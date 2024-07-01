dc8b666475afa80497e124f28c1cee0783546d31
xiaowu55 <jiazewu001@gmail.com>
2024-07-01 22:52:57 +0800

    feat: nav header bar

---
+++ lib/NavList.ts
@@ -0,0 +1,15 @@
+export type ItemList = {
+  Name: string;
+  Link: string;
+  borderColor?: string;
+}[];
+
+
+export const CommunityNavItem: ItemList = [
+  { Name: "MEMBERSHIP", Link: "" },
+  { Name: "MEMBERSHIP", Link: "" },
+  { Name: "MEMBERSHIP", Link: "" },
+  { Name: "MEMBERSHIP", Link: "" },
+  { Name: "MEMBERSHIP", Link: "" },
+  { Name: "MEMBERSHIP", Link: "" },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,105 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  breakpoints: {
-    "3xl": "1920px",
-    "2xl": "1536px",
-    xl: "1280px",
-    lg: "1024px",
-    md: "768px",
-    sm: "640px",
-    xs: "450px",
-  },
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      "nav-origin": "#ff8360",
+      "nav-blue": "#60bcff",
+      "nav-yellow": "#e8e288",
+      "nav-purple": "#ce7db8",
+      "nav-green": "#7dce82",
+      "nav-grey": "#818181",
+      BodyBackground: "#f8f5f2",
+      headline: "#232323",
+      paragraph: "#222525",
+      button: "#078080",
+      buttonText: "#232323",
+      stroke: "#232323",
+      main: "#fffffe",
+      heighLight: "#078080",
+      secondary: "#f45d48",
+      tertiary: "#f8f5f2",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+    safelist:
+      ["nav-origin",
+        "nav-nav-blue",
+        "nav-nav-yellow",
+        "nav-nav-purple",
+        "nav-nav-green",
+        "nav-nav-grey"],
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/forms')],
+} satisfies Config
+
+export default config
